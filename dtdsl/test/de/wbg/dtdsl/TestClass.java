32327c87b3bebe32e491e61625133ca0e43b7f40
Jörn <joern.otte@gmx.net>
2015-03-13 13:16:34 +0100

    - Testclass updated - error Handling - More complex AST-Structure: Head, Node, Attribute

--- dtdsl/test/de/wbg/dtdsl/TestClass.java
+++ dtdsl/test/de/wbg/dtdsl/TestClass.java
@@ -14,20 +14,20 @@ public class TestClass {
 		skv.setI(1);
 		skv.setS("eins");
 		
-		ObjectParser p = new ObjectParser();
-		Node o = p.parse(skv);
-		Node childOfO = o.getChildren().get(0); //key
-		Node childOfChildOfO = childOfO.getChildren().get(0); //value
+		SimpleParser p = new SimpleParser();
+		Head o = p.parse(skv);
+		Node childOfO = (Node)o.getChildren().get(0); //key
+		Attribute childOfChildOfO = (Attribute)childOfO.getChildren().get(0); //value
 		
-		assertTrue("head shouldnt be a key", !o.isKey());
+//		assertTrue("head shouldnt be a key", !o.getType());
 		assertTrue("head should have 1 child, but has: " + o.getChildren().size(), o.getChildren().size() == 1);
 		assertTrue("child of o: should be key", childOfO.isKey());
 		assertTrue("child of o: attribute name should be \"s\"", childOfO.getName() == "s");
-		assertTrue("child of o: wrong parent", childOfO.getParent() == o);
+		assertTrue("child of o: wrong parent", ((Head)childOfO.getParent()) == o);
 		
 		assertTrue("childOfChildOfO: wrong parent", childOfChildOfO.getParent() == childOfO);
 		assertTrue("childOfChildOfO: attribute name should be \"i\"", childOfChildOfO.getName() == "i");
-		assertTrue("childOfChildOfO: should be value", !childOfChildOfO.isKey());
+		assertTrue("childOfChildOfO: should be \"value\", but is ", childOfChildOfO.getType().equals("value"));
 		assertTrue("childOfChildOfO: value should be \"1\", but is " + childOfChildOfO.getValue(), childOfChildOfO.getValue().equals( "1"));
 	}
 	
@@ -44,11 +44,11 @@ public class TestClass {
 		skv2.setS("zwei");
 		skv.setNext(skv2);
 		
-		ObjectParser p = new ObjectParser();
-		Node o = p.parse(skv);
+		SimpleNextParser p = new SimpleNextParser();
+		Head o = p.parse(skv);
 		
-		Node next = o.getChildren().get(1);
-		Node childOfNext = next.getChildren().get(0);
+		Node next = (Node)o.getChildren().get(1);
+		Attribute childOfNext = (Attribute)next.getChildren().get(0);
 		
 		assertTrue("head should have 2 children, but has: " + o.getChildren().size(), o.getChildren().size() == 2);
 		assertTrue("next: should be key", next.isKey());
@@ -58,7 +58,7 @@ public class TestClass {
 		
 		assertTrue("childOfNext: wrong parent", childOfNext.getParent() == next);
 		assertTrue("childOfNext: attribute name should be \"i\"", childOfNext.getName() == "i");
-		assertTrue("childOfNext: should be value", !childOfNext.isKey());
+//		assertTrue("childOfNext: should be value", !childOfNext.isKey());
 		assertTrue("childOfNext: value should be \"2\", but is " + childOfNext.getValue(), childOfNext.getValue().equals( "2"));
 	}
 }
